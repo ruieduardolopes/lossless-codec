6a5d80cfa96058442504e8c4ffdc8d926cc1be4d
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-10-25 00:23:51 +0100

    Start the development of the decoder itself

--- src/Golomb.cpp
+++ src/Golomb.cpp
@@ -6,23 +6,6 @@ Golomb::Golomb(int m) {
     mValue = m;
 }
 
-int Golomb::encode(int n, bstream& file) {
-    int quotient = n / mValue;
-    int remaindr = n % mValue;
-    generate_unary_code(quotient, file);
-    generate_truncated_binary_code(remaindr, file);
-    return 0;
-}
-
-int Golomb::generate_unary_code(int quotient, bstream& file) {
-    int unaryCode = 0;
-    for (int i = 0; i != quotient; i++) { 
-        unaryCode = (unaryCode + 1) << 1; 
-    }
-    file.writeNBits(unaryCode, quotient+1);
-    return unaryCode;
-}
-
 int Golomb::generate_truncated_binary_code(int remaindr, bstream& file) {
     if ((mValue != 0) && ((mValue & (mValue - 1)) == 0)) {
         file.writeNBits(remaindr, 8);                       // code remainder in plain binary
@@ -38,6 +21,47 @@ int Golomb::generate_truncated_binary_code(int remaindr, bstream& file) {
         }
     }
     return 0;
+}
+
+int Golomb::generate_unary_code(int quotient, bstream& file) {
+    int unaryCode = 0;
+    for (int i = 0; i != quotient; i++) { 
+        unaryCode = (unaryCode + 1) << 1; 
+    }
+    file.writeNBits(unaryCode, quotient+1);
+    return unaryCode;
+}
+
+int Golomb::encode(int n, bstream& file) {
+    int quotient = n / mValue;
+    int remaindr = n % mValue;
+    generate_unary_code(quotient, file);
+    generate_truncated_binary_code(remaindr, file);
+    return 0;
+}
+
+int Golomb::decode(bstream& file) {
+    int bValue = ceil(extmath::lb(mValue));
+    int comparisonValue = 2*bValue - mValue; // if there is any problem, check this for a possibility of applying the power, like on the encoder side
+    int numberOfConsecutiveOnes = 0;
+    int readValue;
+    while (true) {
+        readValue = file.readBit();
+        if (readValue == 1) {
+            numberOfConsecutiveOnes++;
+        } else {
+            break;
+        }
+    }
+    int nextInputBits = 0;
+    for (int i = 0; i != bValue; i++) {
+        nextInputBits = (nextInputBits | file.readBit()) << i;
+    }
+    if (nextInputBits < comparisonValue) {
+        // TODO : complete this
+    } else {
+        // TODO : complete this
+    }
 }
 
 int Golomb::endEncode(bstream& file) {
