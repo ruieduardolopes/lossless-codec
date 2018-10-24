e448601050b28138ce88a373c82355b8b093902a
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-10-24 23:53:07 +0100

    Add extended math library to lib directory

---
+++ lib/extmath.cpp
@@ -0,0 +1,5 @@
+#include "extmath.h"
+
+double extmath::lb(int x) {
+    return log10(x) / log10(2);
+}
---
+++ lib/extmath.h
@@ -0,0 +1,11 @@
+#ifndef EXTMATH_H
+#define EXTMATH_H
+
+#include <math.h>
+
+class extmath {
+    public:
+        static double lb(int x);       // binary logarithm of x
+};
+
+#endif
