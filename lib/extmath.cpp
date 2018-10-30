d10712709ce096c3d7894bca9615d700c541a003
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-10-30 12:13:00 +0000

    Add mean, min and minIndex methods to extmath library

--- lib/extmath.cpp
+++ lib/extmath.cpp
@@ -1,5 +1,37 @@
 #include "extmath.h"
 
+#include <iostream>
+
 double extmath::lb(int x) {
     return log10(x) / log10(2);
+}
+
+double extmath::mean(vector<short> set) {
+    double sum;
+    for (auto element : set) {
+        sum += element;
+    }
+    return sum/set.size();
+}
+
+short extmath::min(vector<short> set) {
+    short minimum = INT16_MAX;
+    for (auto element : set) {
+        if (element < minimum) {
+            minimum = element;
+        }
+    }
+    return minimum;
+}
+
+short extmath::minIndex(vector<double> set) {
+    short minimum = INT16_MAX;
+    short minimumIndex;
+    for (int index = 0; index != set.size(); index++) {
+        if (set[index] < minimum) {
+            minimum = set[index];
+            minimumIndex = index;
+        }
+    }
+    return minimumIndex;
 }
--- lib/extmath.h
+++ lib/extmath.h
@@ -1,11 +1,18 @@
 #ifndef EXTMATH_H
 #define EXTMATH_H
 
+#include <climits>
 #include <math.h>
+#include <vector>
+
+using namespace std;
 
 class extmath {
     public:
-        static double lb(int x);       // binary logarithm of x
+        static double lb(int x);                    // binary logarithm of x
+        static double mean(vector<short> set);      // mean value of a short vector
+        static short min(vector<short> set);        // minimum value of a short vector
+        static short minIndex(vector<double> set);  // minimum value index of a double vector
 };
 
 #endif
