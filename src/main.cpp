48cb5572b3e47e2a04d7ec499d09185e1c9d069f
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-11-03 21:13:41 +0000

    Test (successfully) the predictions with the Golomb Coding

--- src/main.cpp
+++ src/main.cpp
@@ -30,10 +30,52 @@ int main(void) {
             predictor.predict();
         }
     }
+    vector<short> predictedSamples = predictor.getPredictedSamples();
+    cout << "length: " << predictedSamples.size() << endl;
+    bstream output = bstream{ "output.wavz", ios::out|ios::binary };
+    Golomb golomb = Golomb(8);
+    for (auto sample : predictedSamples) {
+        // cout << sample << ", ";
+        golomb.encode(sample, output);
+    }
+    // cout << endl;
+    golomb.endEncode(output);
+    // --- file should be created
+
+    bstream input = bstream{ "output.wavz", ios::in|ios::binary };
+    vector<short> fromFile;
+    cout << "DECODED:" << endl;
+    for (int i = 0; i != predictedSamples.size(); i++) {
+        fromFile.push_back(golomb.decode(input));
+        // cout << fromFile[i] << ", ";
+    }
+    // cout << endl;
+    cout << "length: " << fromFile.size() << endl;
+    //predictor.setPredictedSamples(predictedSamples);
+
     predictor.revert();
     samples = predictor.getRevertSamples();
     audio.save("file.compressed.wav", samples, nFrames);
 
     return 0;
+
+    /*Golomb golomb = Golomb(3);
+    bstream file = bstream{ "output", ios::out|ios::binary };
+    for (int i = -11; i != 11; i++) {
+        cout << "value " << i << endl;
+        golomb.encode(i, file);
+    }
+    golomb.endEncode(file);
+    cout << "DECODE" << endl;
+    bstream file2 = bstream{ "output", ios::in|ios::binary };
+    for (int i = -11; i != 11; i++) {
+        cout << "value " << i << " is " << golomb.decode(file2) << endl;
+    }
+    file2.grantWrite();
+
+
+    bstream test = bstream{ "test" , ios::out|ios::binary};
+    test.writeNBits(1, 10);
+    test.grantWrite();*/
 }
 
